db6a127b382ca026388bccaff392145444b93189
henrik <>
2022-04-02 11:41:35 +0200

    Add flag to remove compiler version from binary

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 flags="-Wall -Wextra -Wconversion -Wshadow -Wpadded -Werror -Wno-maybe-uninitialized -Wno-unknown-warning-option -Wno-unused-command-line-argument \
--fno-pie -nostdlib -ffreestanding -static -fno-asynchronous-unwind-tables -fcf-protection=none -fno-stack-protector \
--nostartfiles -Wl,--gc-sections -Wl,--build-id=none"
+-fno-pie -nostdlib -ffreestanding -static -fno-asynchronous-unwind-tables -fcf-protection=none -fno-stack-protector -Qn \
+-nostartfiles -Wl,--gc-sections -Wl,--build-id=none" # -Wl,--no-rosegment
 debug_flags="$flags -fsanitize-undefined-trap-on-error -fsanitize=undefined -g -O2 $FLAGS"
 release_flags="$flags -Ddebug_NDEBUG -O2 -s $FLAGS"
 CC=${CC:-gcc}
@@ -9,3 +9,4 @@ $CC $debug_flags -S -o debug.bin.s main.c
 $CC $debug_flags -o debug.bin main.c
 $CC $release_flags -S -o release.bin.s main.c
 $CC $release_flags -o release.bin main.c
+$CC $release_flags -c -o release.bin.o main.c
