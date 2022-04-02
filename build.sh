7dbc6b133d4c6fa1912b41bf45ef71fe79c1c5c4
henrik <>
2022-04-02 11:42:28 +0200

    Fix previous commit

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -e
 flags="-Wall -Wextra -Wconversion -Wshadow -Wpadded -Werror -Wno-maybe-uninitialized -Wno-unknown-warning-option -Wno-unused-command-line-argument \
 -fno-pie -nostdlib -ffreestanding -static -fno-asynchronous-unwind-tables -fcf-protection=none -fno-stack-protector -Qn \
--nostartfiles -Wl,--gc-sections -Wl,--build-id=none" # -Wl,--no-rosegment
+-nostartfiles -Wl,--gc-sections -Wl,--build-id=none"
 debug_flags="$flags -fsanitize-undefined-trap-on-error -fsanitize=undefined -g -O2 $FLAGS"
 release_flags="$flags -Ddebug_NDEBUG -O2 -s $FLAGS"
 CC=${CC:-gcc}
@@ -9,4 +9,3 @@ $CC $debug_flags -S -o debug.bin.s main.c
 $CC $debug_flags -o debug.bin main.c
 $CC $release_flags -S -o release.bin.s main.c
 $CC $release_flags -o release.bin main.c
-$CC $release_flags -c -o release.bin.o main.c
