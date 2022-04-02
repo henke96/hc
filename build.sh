da32a0c2233c89ff8206b659a188a6a896fd1f12
henrik <>
2022-04-02 09:56:26 +0200

    Update build flags

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 set -e
 flags="-Wall -Wextra -Wconversion -Wshadow -Wpadded -Werror -Wno-maybe-uninitialized -Wno-unknown-warning-option -Wno-unused-command-line-argument \
--fno-pie -nostdlib -ffreestanding -static -fno-asynchronous-unwind-tables \
+-fno-pie -nostdlib -ffreestanding -static -fno-asynchronous-unwind-tables -fcf-protection=none -fno-stack-protector \
 -nostartfiles -Wl,--gc-sections -Wl,--build-id=none"
 debug_flags="$flags -fsanitize-undefined-trap-on-error -fsanitize=undefined -g -O2 $FLAGS"
 release_flags="$flags -Ddebug_NDEBUG -O2 -s $FLAGS"
 CC=${CC:-gcc}
-$CC $debug_flags -fverbose-asm -S -o debug.bin.s main.c
+$CC $debug_flags -S -o debug.bin.s main.c
 $CC $debug_flags -o debug.bin main.c
-$CC $release_flags -fverbose-asm -S -o release.bin.s main.c
+$CC $release_flags -S -o release.bin.s main.c
 $CC $release_flags -o release.bin main.c
--- examples/hello/main.c
+++ examples/hello/main.c
@@ -4,7 +4,6 @@
 #include "../../hc/syscalls.c"
 
 int32_t main(hc_UNUSED int32_t argc, hc_UNUSED char **argv) {
-    static const char message[7] = "Hello!\n";
-    hc_write(STDOUT_FILENO, &message[0], sizeof(message));
+    hc_write(STDOUT_FILENO, "Hello!\n", 7);
     return 0;
 }
