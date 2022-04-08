2579ef3e46b3a765ec02d9a1826ad2999b22627f
henrik <>
2022-04-08 22:37:07 +0200

    wasmJs example: rename add -> addAndPrint

--- examples/wasmJs/main.c
+++ examples/wasmJs/main.c
@@ -4,7 +4,7 @@ hc_WASM_IMPORT("env", "print")
 void print(int32_t number);
 
 hc_WASM_EXPORT("addAndPrint")
-int32_t add(int32_t a, int32_t b) {
+int32_t addAndPrint(int32_t a, int32_t b) {
     int32_t result = a + b;
     print(result);
     return result;
