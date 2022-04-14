638515e6eb0ce65b07afb25600d54b24bafadc7c
henrik <>
2022-04-14 17:59:01 +0200

    os example: Make bootloader smaller, it doesn't have to be fast

--- examples/os/build.sh
+++ examples/os/build.sh
@@ -9,6 +9,6 @@ flags="-Wl,-T$script_dir/kernel/kernel.ld -mno-red-zone -O2 -s"
 llvm-objcopy -O binary "$script_dir/kernel/kernel.bin.elf" "$script_dir/kernel/kernel.bin"
 
 # Bootloader (with kernel binary embedded)
-flags="-Wl,-subsystem:efi_application -I$script_dir/kernel -O2 -s"
+flags="-Wl,-subsystem:efi_application -I$script_dir/kernel -Os -s"
 "$script_dir/../../cc_pe.sh" $flags -S -o "$script_dir/bootloader/bootloader.efi.s" "$script_dir/bootloader/main.c"
 "$script_dir/../../cc_pe.sh" $flags -o "$script_dir/bootloader.efi" "$script_dir/bootloader/main.c"
