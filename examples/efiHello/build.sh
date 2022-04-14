89f8c4217fcacb0f9abc0e1423488504e0aebf43
henrik <>
2022-04-14 17:14:28 +0200

    Always build assembly first

--- examples/efiHello/build.sh
+++ examples/efiHello/build.sh
@@ -2,5 +2,5 @@
 set -e
 script_dir="$(dirname $0)"
 flags="-Wl,-subsystem:efi_application -O2 -s $FLAGS"
-"$script_dir/../../cc_pe.sh" $flags -o "$script_dir/release.efi" "$script_dir/main.c"
 "$script_dir/../../cc_pe.sh" $flags -S -o "$script_dir/release.efi.s" "$script_dir/main.c"
+"$script_dir/../../cc_pe.sh" $flags -o "$script_dir/release.efi" "$script_dir/main.c"
--- examples/wasmHello/build.sh
+++ examples/wasmHello/build.sh
@@ -2,5 +2,5 @@
 set -e
 script_dir="$(dirname $0)"
 flags="-O2 -s $FLAGS"
-"$script_dir/../../cc_wasm.sh" $flags -o "$script_dir/release.wasm" "$script_dir/main.c"
 "$script_dir/../../cc_wasm.sh" $flags -S -o "$script_dir/release.wasm.s" "$script_dir/main.c"
+"$script_dir/../../cc_wasm.sh" $flags -o "$script_dir/release.wasm" "$script_dir/main.c"
