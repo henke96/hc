dc4cb27c83b173ffb29890320a9ad2bfa851c072
henrik <>
2022-05-03 19:29:32 +0200

    gnu example: Call __libc_start_main
    Maybe not needed with glibc, but musl initialises globals such as program_invocation_name

--- examples/gnuHello/main.c
+++ examples/gnuHello/main.c
@@ -7,7 +7,12 @@
 #include "../../src/linux/helpers/_start.c"
 #include "../../src/linux/gnulinux/libdl.h"
 
-static noreturn void stage1(uint64_t *auxv) {
+static void *libcHandle;
+
+static noreturn void stage1(int32_t argc, char **argv) {
+    char **envp = util_getEnvp(argc, argv);
+    uint64_t *auxv = util_getAuxv(envp);
+
     // Find program path.
     char *programPath;
     for (uint64_t *current = auxv; current[0] != AT_NULL; current += 2) {
@@ -20,66 +25,66 @@ static noreturn void stage1(uint64_t *auxv) {
     sys_exit(1);
 
     foundProgramPath:;
-    const char *argv[3];
-    argv[1] = programPath;
-    argv[2] = NULL;
-    const char *envp[] = {
+    const char *newArgv[3];
+    newArgv[1] = programPath;
+    newArgv[2] = NULL;
+    const char *newEnvp[] = {
         "STAGE2=1",
         NULL
     };
     // Try known dynamic linker paths.
 #if hc_X86_64
-    argv[0] = "/lib64/ld-linux-x86-64.so.2";
-    sys_execveat(-1, argv[0], &argv[0], &envp[0], 0);
-    argv[0] = "/lib/ld-musl-x86_64.so.1";
-    sys_execveat(-1, argv[0], &argv[0], &envp[0], 0);
+    newArgv[0] = "/lib64/ld-linux-x86-64.so.2";
+    sys_execveat(-1, newArgv[0], &newArgv[0], &newEnvp[0], 0);
+    newArgv[0] = "/lib/ld-musl-x86_64.so.1";
+    sys_execveat(-1, newArgv[0], &newArgv[0], &newEnvp[0], 0);
 #elif hc_AARCH64
-    argv[0] = "/lib/ld-linux-aarch64.so.1";
-    sys_execveat(-1, argv[0], &argv[0], &envp[0], 0);
-    argv[0] = "/lib/ld-musl-aarch64.so.1";
-    sys_execveat(-1, argv[0], &argv[0], &envp[0], 0);
+    newArgv[0] = "/lib/ld-linux-aarch64.so.1";
+    sys_execveat(-1, newArgv[0], &newArgv[0], &newEnvp[0], 0);
+    newArgv[0] = "/lib/ld-musl-aarch64.so.1";
+    sys_execveat(-1, newArgv[0], &newArgv[0], &newEnvp[0], 0);
 #elif hc_RISCV64
-    argv[0] = "/lib/ld-linux-riscv64-lp64d.so.1"; // Hard floats.
-    sys_execveat(-1, argv[0], &argv[0], &envp[0], 0);
-    argv[0] = "/lib/ld-linux-riscv64-lp64.so.1";
-    sys_execveat(-1, argv[0], &argv[0], &envp[0], 0);
-    argv[0] = "/lib/ld-musl-riscv64.so.1";
-    sys_execveat(-1, argv[0], &argv[0], &envp[0], 0);
+    newArgv[0] = "/lib/ld-linux-riscv64-lp64d.so.1"; // Hard floats.
+    sys_execveat(-1, newArgv[0], &newArgv[0], &newEnvp[0], 0);
+    newArgv[0] = "/lib/ld-linux-riscv64-lp64.so.1";
+    sys_execveat(-1, newArgv[0], &newArgv[0], &newEnvp[0], 0);
+    newArgv[0] = "/lib/ld-musl-riscv64.so.1";
+    sys_execveat(-1, newArgv[0], &newArgv[0], &newEnvp[0], 0);
 #endif
     sys_exit(1);
 }
 
+static int32_t libcMain(hc_UNUSED int32_t argc, hc_UNUSED char **argv) {
+    int32_t (*printf)(const char *restrict format, ...) = dlsym(libcHandle, "printf");
+    if (dlerror() != NULL) return 1;
+    printf("Hello world!\n");
+    return 0;
+}
+
 int32_t main(int32_t argc, char **argv) {
     char **envp = util_getEnvp(argc, argv);
-    uint64_t *auxv = util_getAuxv(envp);
-
     // Check if we have re-executed ourself through the dynamic linker yet (stage2).
     for (char **current = envp; *current != NULL; ++current) {
         if (util_cstrCmp(*current, "STAGE2=1") == 0) goto stage2; // Yep.
     }
     // Nope.
-    stage1(auxv);
+    stage1(argc, argv);
     hc_UNREACHABLE;
 
     stage2:;
-    void *handle = dlopen("libc.so.6", RTLD_NOW);
-    char *error = dlerror();
-    if (error != NULL) {
-        sys_writev(STDOUT_FILENO, (struct iovec[2]) {
-            { .iov_base = &error[0],    .iov_len = util_cstrLen(error) },
-            { .iov_base = (char *)"\n", .iov_len = 1 }
-        }, 2);
-        return 1;
-    }
-    int32_t (*printf)(const char *restrict format, ...) = dlsym(handle, "printf");
-    error = dlerror();
-    if (error != NULL) {
-        sys_writev(STDOUT_FILENO, (struct iovec[2]) {
-            { .iov_base = &error[0],    .iov_len = util_cstrLen(error) },
-            { .iov_base = (char *)"\n", .iov_len = 1 }
-        }, 2);
-        return 1;
-    }
-    printf("Hello world!\n");
-    return 0;
+    libcHandle = dlopen("libc.so.6", RTLD_NOW);
+    if (dlerror() != NULL) return 1;
+
+    void (*__libc_start_main)(
+        void *main,
+        int32_t argc,
+        char **argv,
+        void *init,
+        void *fini,
+        void *rtldFini
+    ) = dlsym(libcHandle, "__libc_start_main");
+    if (dlerror() != NULL) return 1;
+
+    __libc_start_main(libcMain, argc, argv, NULL, NULL, NULL);
+    return 1;
 }
