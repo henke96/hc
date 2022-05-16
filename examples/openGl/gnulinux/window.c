eb5676cbe4b87d2c4d815e8b9e0e803fc9f192ed
henrik <>
2022-05-16 20:51:50 +0200

    Revert accidental change

--- examples/openGl/gnulinux/window.c
+++ examples/openGl/gnulinux/window.c
@@ -30,7 +30,7 @@ static int32_t window_init(struct window *self, char **envp) {
         egl_CONTEXT_MINOR_VERSION, 0,
         egl_NONE
     };
-    status = egl_createContext(&self->egl, egl_OPENGL_API, &configAttributes[0], &contextAttributes[0]);
+    status = egl_createContext(&self->egl, egl_OPENGL_ES_API, &configAttributes[0], &contextAttributes[0]);
     if (status < 0) {
         printf("Failed to create EGL context (%d)\n", status);
         goto cleanup_egl;
