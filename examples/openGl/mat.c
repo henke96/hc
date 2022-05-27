81bc4922e75451367a3a8df31b45dc83c406a875
henrik <>
2022-05-27 13:55:18 +0200

    opengl example: Add more rotations

--- examples/openGl/mat.c
+++ examples/openGl/mat.c
@@ -27,9 +27,35 @@ static void mat_translate(float *mat, float x, float y, float z) {
     mat[14] += z;
 }
 
+hc_UNUSED
+static void mat_rotateX(float *mat, uint32_t pitchAngle) {
+    float sin = trig_sin(pitchAngle);
+    float cos = trig_cos(pitchAngle);
+    float temp1 = mat[1];
+    float temp2 = mat[2];
+    mat[1] = cos * temp1 - sin * temp2;
+    mat[2] = cos * temp1 + sin * temp2;
+
+    temp1 = mat[5];
+    temp2 = mat[6];
+    mat[5] = cos * temp1 - sin * temp2;
+    mat[6] = cos * temp1 + sin * temp2;
+
+    temp1 = mat[9];
+    temp2 = mat[10];
+    mat[9] = cos * temp1 - sin * temp2;
+    mat[10] = cos * temp1 + sin * temp2;
+
+    temp1 = mat[13];
+    temp2 = mat[14];
+    mat[13] = cos * temp1 - sin * temp2;
+    mat[14] = cos * temp1 + sin * temp2;
+}
+
+hc_UNUSED
 static void mat_rotateY(float *mat, uint32_t yawAngle) {
-    float sin = trig_sin(angle);
-    float cos = trig_cos(angle);
+    float sin = trig_sin(yawAngle);
+    float cos = trig_cos(yawAngle);
     float temp1 = mat[0];
     float temp2 = mat[2];
     mat[0] = cos * temp1 + sin * temp2;
@@ -49,6 +75,31 @@ static void mat_rotateY(float *mat, uint32_t yawAngle) {
     temp2 = mat[14];
     mat[12] = cos * temp1 + sin * temp2;
     mat[14] = cos * temp2 - sin * temp1;
+}
+
+hc_UNUSED
+static void mat_rotateZ(float *mat, uint32_t rollAngle) {
+    float sin = trig_sin(rollAngle);
+    float cos = trig_cos(rollAngle);
+    float temp1 = mat[0];
+    float temp2 = mat[1];
+    mat[0] = cos * temp1 - sin * temp2;
+    mat[1] = cos * temp2 + sin * temp1;
+
+    temp1 = mat[4];
+    temp2 = mat[5];
+    mat[4] = cos * temp1 - sin * temp2;
+    mat[5] = cos * temp2 + sin * temp1;
+
+    temp1 = mat[8];
+    temp2 = mat[9];
+    mat[8] = cos * temp1 - sin * temp2;
+    mat[9] = cos * temp2 + sin * temp1;
+
+    temp1 = mat[12];
+    temp2 = mat[13];
+    mat[12] = cos * temp1 - sin * temp2;
+    mat[13] = cos * temp2 + sin * temp1;
 }
 
 // Left handed projection matrix: +X is right, +Y is up, +Z is forward.
