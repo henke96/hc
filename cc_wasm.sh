d5a23b775f41cbec487d9bca7bec3ec38eb8f511
henrik <>
2022-11-21 21:10:20 +0100

    Use --ld-path instead of -fuse-ld

--- cc_wasm.sh
+++ cc_wasm.sh
@@ -3,5 +3,5 @@ set -e
 script_dir="$(dirname $0)"
 flags="$(cat $script_dir/flags)"
 CC="${CC:-clang}"
-LD="${LD:-lld}"
-"$CC" -I$script_dir/src $flags -target wasm32-unknown-unknown-unknown -fuse-ld="$LD" "$@"
+LD="${LD:-ld.lld}"
+"$CC" -I$script_dir/src $flags -target wasm32-unknown-unknown-unknown --ld-path="$LD" "$@"
--- src/hc/linux/gnulinux/dynamic/gen_lib.bat
+++ src/hc/linux/gnulinux/dynamic/gen_lib.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 if not defined CC set CC=clang
-if not defined LD set LD=lld
+if not defined LD set LD=ld.lld
 if not defined ARCH set ARCH=x86_64
-%CC% -target %ARCH%-unknown-linux-elf -fuse-ld="%LD%" -shared -nostdlib -o %2%1 %~dp0%1.c
+%CC% -target %ARCH%-unknown-linux-elf --ld-path="%LD%" -shared -nostdlib -o %2%1 %~dp0%1.c
 endlocal
