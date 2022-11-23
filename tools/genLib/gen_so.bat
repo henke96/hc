370e4e788d8e79bdc771f6e4920702415623f16c
henrik <>
2022-11-23 21:12:05 +0100

    Move .so generate script to tools/

--- tools/genLib/gen_so.bat
+++ tools/genLib/gen_so.bat
@@ -3,5 +3,5 @@ setlocal
 if not defined CC set CC=clang
 if not defined LD set LD=ld.lld
 if not defined ARCH set ARCH=x86_64
-%CC% -target %ARCH%-unknown-linux-elf --ld-path="%LD%" -shared -nostdlib -o %2%1 %~dp0%1.c
+%CC% -target %ARCH%-unknown-linux-elf --ld-path="%LD%" -shared -nostdlib -o "%2" "%1"
 endlocal
