8653ade6a054aeec5a0942b4ce3b1bbba47a1a3d
henrik <>
2023-05-02 20:33:53 +0200

    Small cleanup

--- src/hc/hc.h
+++ src/hc/hc.h
@@ -164,7 +164,7 @@ typedef long long int64_t;
 #define offsetof __builtin_offsetof
 
 typedef __builtin_va_list va_list;
-#define va_start(ap, param) __builtin_va_start(ap, param)
-#define va_end(ap) __builtin_va_end(ap)
-#define va_arg(ap, type) __builtin_va_arg(ap, type)
-#define va_copy(dest, src) __builtin_va_copy(dest, src)
+#define va_start __builtin_va_start
+#define va_end __builtin_va_end
+#define va_arg __builtin_va_arg
+#define va_copy __builtin_va_copy
