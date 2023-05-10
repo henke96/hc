fbb0f18c4808f579465960047ae3d281fc2227e6
henrik <>
2023-05-10 22:40:04 +0200

    Remove some tabs

--- src/hc/freebsd/_start.c
+++ src/hc/freebsd/_start.c
@@ -3,10 +3,10 @@ hc_ELF_EXPORT char *__progname = "";
 
 #if hc_X86_64
 void _start(char **ap, void (*cleanup)(void)) {
-	int32_t argc = *(int32_t *)ap;
-	char **argv = &ap[1];
-	char **env = &ap[2 + argc];
-	__libc_start1(argc, argv, env, cleanup, start);
+    int32_t argc = *(int32_t *)ap;
+    char **argv = &ap[1];
+    char **env = &ap[2 + argc];
+    __libc_start1(argc, argv, env, cleanup, start);
 }
 #else
 // TODO aarch64
