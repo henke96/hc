04c02201a84546c2617bde3f11e37633a2a154b5
henrik <>
2023-05-16 21:07:02 +0200

    Fix jni example build

--- examples/jni/linux/build.bat
+++ examples/jni/linux/build.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal disabledelayedexpansion
-set "root_dir=%~dp0..\..\"
+set "root_dir=%~dp0..\..\..\"
 
 set "FLAGS=-shared -fPIC %FLAGS%"
-call "%root_dir%tools\build\elf.bat" "%~dp0linux\" libtest so
+call "%root_dir%tools\build\elf.bat" "%~dp0" libtest so
--- examples/jni/linux/build.sh
+++ examples/jni/linux/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 script_dir="$(dirname "$0")"
-root_dir="$script_dir/../.."
+root_dir="$script_dir/../../.."
 
-FLAGS="-shared -fPIC $FLAGS" "$root_dir/tools/build/elf.sh" "$script_dir/linux" libtest so
+FLAGS="-shared -fPIC $FLAGS" "$root_dir/tools/build/elf.sh" "$script_dir" libtest so
