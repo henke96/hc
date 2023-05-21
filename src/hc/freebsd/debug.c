82eecdb4c465d407ffdd102f0694449fb953837b
henrik <>
2023-05-21 11:35:20 +0200

    Add some inline attributes

--- src/hc/freebsd/debug.c
+++ src/hc/freebsd/debug.c
@@ -9,22 +9,22 @@
 #endif
 
 hc_UNUSED
-static noreturn hc_COLD void debug_abort(void) {
+static noreturn hc_INLINE void debug_abort(void) {
     abort();
 }
 
 hc_UNUSED
-static noreturn hc_COLD void debug_fail(int64_t res, const char *expression, const char *file, int32_t line) {
+static noreturn void debug_fail(int64_t res, const char *expression, const char *file, int32_t line) {
     printf("%s:%d fail: %s = %lld\n", file, line, expression, res);
     debug_abort();
 }
 
 hc_UNUSED
-static void hc_COLD debug_print(const char *str) {
+static void hc_INLINE debug_print(const char *str) {
     printf(str);
 }
 
 hc_UNUSED
-static void hc_COLD debug_printNum(const char *pre, int64_t num, const char *post) {
+static void hc_INLINE debug_printNum(const char *pre, int64_t num, const char *post) {
     printf("%s%lld%s", pre, num, post);
 }
