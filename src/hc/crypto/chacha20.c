7dcc723d44c3142eb57d4777444d6aadc638ef89
henrik <>
2023-07-03 22:08:53 +0200

    Clean up chacha20 implementation

--- src/hc/crypto/chacha20.c
+++ src/hc/crypto/chacha20.c
@@ -24,35 +24,36 @@ union chacha20 {
     C += D; B ^= C; B = hc_ROTL32(B, 7);
 
 static void chacha20_block(const union chacha20 *self, union chacha20 *outStream) {
-    hc_MEMCPY(&outStream->u8[0], &self->u8[0], 64);
+    union chacha20 local;
+    hc_MEMCPY(&local.u64[0], &self->u64[0], 64);
 
     for (int32_t i = 0; i < 10; ++i) {
-        _chacha20_QUARTER_ROUND(outStream->u32[0], outStream->u32[4], outStream->u32[8], outStream->u32[12])
-        _chacha20_QUARTER_ROUND(outStream->u32[1], outStream->u32[5], outStream->u32[9], outStream->u32[13])
-        _chacha20_QUARTER_ROUND(outStream->u32[2], outStream->u32[6], outStream->u32[10], outStream->u32[14])
-        _chacha20_QUARTER_ROUND(outStream->u32[3], outStream->u32[7], outStream->u32[11], outStream->u32[15])
-        _chacha20_QUARTER_ROUND(outStream->u32[0], outStream->u32[5], outStream->u32[10], outStream->u32[15])
-        _chacha20_QUARTER_ROUND(outStream->u32[1], outStream->u32[6], outStream->u32[11], outStream->u32[12])
-        _chacha20_QUARTER_ROUND(outStream->u32[2], outStream->u32[7], outStream->u32[8], outStream->u32[13])
-        _chacha20_QUARTER_ROUND(outStream->u32[3], outStream->u32[4], outStream->u32[9], outStream->u32[14])
+        _chacha20_QUARTER_ROUND(local.u32[0], local.u32[4], local.u32[8], local.u32[12])
+        _chacha20_QUARTER_ROUND(local.u32[1], local.u32[5], local.u32[9], local.u32[13])
+        _chacha20_QUARTER_ROUND(local.u32[2], local.u32[6], local.u32[10], local.u32[14])
+        _chacha20_QUARTER_ROUND(local.u32[3], local.u32[7], local.u32[11], local.u32[15])
+        _chacha20_QUARTER_ROUND(local.u32[0], local.u32[5], local.u32[10], local.u32[15])
+        _chacha20_QUARTER_ROUND(local.u32[1], local.u32[6], local.u32[11], local.u32[12])
+        _chacha20_QUARTER_ROUND(local.u32[2], local.u32[7], local.u32[8], local.u32[13])
+        _chacha20_QUARTER_ROUND(local.u32[3], local.u32[4], local.u32[9], local.u32[14])
     }
 
-    outStream->u32[0] += self->u32[0];
-    outStream->u32[1] += self->u32[1];
-    outStream->u32[2] += self->u32[2];
-    outStream->u32[3] += self->u32[3];
-    outStream->u32[4] += self->u32[4];
-    outStream->u32[5] += self->u32[5];
-    outStream->u32[6] += self->u32[6];
-    outStream->u32[7] += self->u32[7];
-    outStream->u32[8] += self->u32[8];
-    outStream->u32[9] += self->u32[9];
-    outStream->u32[10] += self->u32[10];
-    outStream->u32[11] += self->u32[11];
-    outStream->u32[12] += self->u32[12];
-    outStream->u32[13] += self->u32[13];
-    outStream->u32[14] += self->u32[14];
-    outStream->u32[15] += self->u32[15];
+    outStream->u32[0] = local.u32[0] + self->u32[0];
+    outStream->u32[1] = local.u32[1] + self->u32[1];
+    outStream->u32[2] = local.u32[2] + self->u32[2];
+    outStream->u32[3] = local.u32[3] + self->u32[3];
+    outStream->u32[4] = local.u32[4] + self->u32[4];
+    outStream->u32[5] = local.u32[5] + self->u32[5];
+    outStream->u32[6] = local.u32[6] + self->u32[6];
+    outStream->u32[7] = local.u32[7] + self->u32[7];
+    outStream->u32[8] = local.u32[8] + self->u32[8];
+    outStream->u32[9] = local.u32[9] + self->u32[9];
+    outStream->u32[10] = local.u32[10] + self->u32[10];
+    outStream->u32[11] = local.u32[11] + self->u32[11];
+    outStream->u32[12] = local.u32[12] + self->u32[12];
+    outStream->u32[13] = local.u32[13] + self->u32[13];
+    outStream->u32[14] = local.u32[14] + self->u32[14];
+    outStream->u32[15] = local.u32[15] + self->u32[15];
 }
 
 hc_UNUSED
