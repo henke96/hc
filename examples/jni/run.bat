0dcecce043922b6fc2cab8747de1518ad8014237
Henke <>
2023-07-14 17:50:59 +0200

    Fix jni example on windows

--- examples/jni/run.bat
+++ examples/jni/run.bat
@@ -2,9 +2,8 @@
 setlocal disabledelayedexpansion
 set "script_dir=%~dp0"
 set "script_dir=%script_dir:~0,-1%"
-set "root_dir=%script_dir%\..\..\.."
-
-call "%root_dir%tools\shellUtil\setnativearch.bat"
+set "root_dir=%script_dir%\..\.."
+call "%root_dir%\tools\shellUtil\setnativearch.bat"
 if not errorlevel 0 exit /b
 if errorlevel 1 exit /b
 
