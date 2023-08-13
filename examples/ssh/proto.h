b48483623ec3ddda388924f887c9720011987366
henrik <>
2023-08-13 09:22:26 +0200

    Add more ssh constants

--- examples/ssh/proto.h
+++ examples/ssh/proto.h
@@ -6,5 +6,26 @@
 #define proto_MSG_SERVICE_ACCEPT 6
 #define proto_MSG_KEXINIT 20
 #define proto_MSG_NEWKEYS 21
+
 #define proto_MSG_KEX_ECDH_INIT 30
 #define proto_MSG_KEX_ECDH_REPLY 31
+
+#define proto_MSG_USERAUTH_REQUEST 50
+#define proto_MSG_USERAUTH_FAILURE 51
+#define proto_MSG_USERAUTH_SUCCESS 52
+#define proto_MSG_USERAUTH_BANNER 53
+
+#define proto_MSG_GLOBAL_REQUEST 80
+#define proto_MSG_REQUEST_SUCCESS 81
+#define proto_MSG_REQUEST_FAILURE 82
+#define proto_MSG_CHANNEL_OPEN 90
+#define proto_MSG_CHANNEL_OPEN_CONFIRMATION 91
+#define proto_MSG_CHANNEL_OPEN_FAILURE 92
+#define proto_MSG_CHANNEL_WINDOW_ADJUST 93
+#define proto_MSG_CHANNEL_DATA 94
+#define proto_MSG_CHANNEL_EXTENDED_DATA 95
+#define proto_MSG_CHANNEL_EOF 96
+#define proto_MSG_CHANNEL_CLOSE 97
+#define proto_MSG_CHANNEL_REQUEST 98
+#define proto_MSG_CHANNEL_SUCCESS 99
+#define proto_MSG_CHANNEL_FAILURE 100
