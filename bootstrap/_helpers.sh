f69a0796f605b8c7569923a17f5a9c10f6be5ff3
henrik <>
2023-08-20 12:07:21 +0200

    Make bootstrap more portable

--- bootstrap/_helpers.sh
+++ bootstrap/_helpers.sh
@@ -2,7 +2,7 @@ fetch() {
     set +e
     test -f "$2"
     if test $? -ne 0; then
-        wget -nv -nc -O temp "$1$2" && mv temp "$2"
+        wget -O temp "$1$2" && mv temp "$2"
         if test $? -ne 0; then
             echo "Failed to download $1$2"
             echo "Please fetch $(pwd)/$2 manually, then type OK to continue"
@@ -14,27 +14,25 @@ fetch() {
 }
 
 extract_and_enter() {
-    mkdir temp
-    decrypt_opt=""
-    if test "$2" = ".tar.gz"; then
-        decrypt_opt="-z"
-    elif test "$2" = ".tar.xz"; then
-        decrypt_opt="-J"
-    fi
     set +e
     test -d "$1"
     if test $? -ne 0; then
-        tar x -C temp -k "$decrypt_opt" -f "$1$2" && mv "temp/$1" "$1"
-        if test $? -ne 0; then
-            echo "Failed to extract $1$2"
-            echo "Please extract $(pwd)/$1 manually, then type OK to continue"
-            read answer
-            test "$answer" = "OK" || exit 1
-            rm -rf "temp/$1"
+        set -e
+        mkdir temp
+        cd temp
+        if test "$2" = ".tar.gz"; then
+            gzip -d -c "../$1$2" | tar xf - && mv "$1" "../$1"
+        elif test "$2" = ".tar.xz"; then
+            xz -d -c "../$1$2" | tar xf - && mv "$1" "../$1"
+        elif test "$2" = ".tar.bz2"; then
+            bzip2 -d -c "../$1$2" | tar xf - && mv "$1" "../$1"
+        else
+            tar xf "../$1$2" && mv "$1" "../$1"
         fi
+        cd ..
+        rmdir temp
     fi
     set -e
-    rmdir temp
     cd "$1"
 }
 
