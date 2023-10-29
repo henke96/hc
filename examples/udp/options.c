d195ad2e5abb3c8de21c351275e346a45e4a54e2
henrik <>
2023-10-29 13:42:32 +0100

    Add udp example program

---
+++ examples/udp/options.c
@@ -0,0 +1,88 @@
+struct options {
+    char *interface;
+    int32_t interfaceLen;
+    uint16_t port;
+    uint8_t bindAddress[4];
+    bool broadcast;
+    char __pad[5];
+};
+
+static int32_t options_init(struct options *self, int32_t argc, char **argv) {
+    *self = (struct options) {
+        .interface = NULL,
+        .interfaceLen = 0,
+        .port = 7777,
+        .bindAddress = { 0, 0, 0, 0 },
+        .broadcast = false,
+    };
+
+    char prevOpt = '\0';
+    while (--argc > 0) {
+        char *arg = *++argv;
+        switch (prevOpt) {
+            case 'B': {
+                for (int32_t i = 0;;) {
+                    uint64_t octet;
+                    int32_t parsed = util_strToUint(arg, INT32_MAX, &octet);
+                    if (parsed <= 0 || octet > 255) goto optsDone;
+                    self->bindAddress[i] = (uint8_t)octet;
+
+                    if (++i == hc_ARRAY_LEN(self->bindAddress)) {
+                        if (arg[parsed] != '\0') goto optsDone;
+                        break;
+                    } else if (arg[parsed] != '.') goto optsDone;
+                    arg += parsed + 1;
+                }
+                break;
+            }
+            case 'p': {
+                uint64_t port;
+                int32_t parsed = util_strToUint(arg, INT32_MAX, &port);
+                if (parsed <= 0 || port > UINT16_MAX || arg[parsed] != '\0') goto optsDone;
+                self->port = (uint16_t)port;
+                break;
+            }
+            case 'i': {
+                int64_t ifLen = util_cstrLen(arg);
+                if (ifLen > INT32_MAX) goto optsDone;
+                self->interface = arg;
+                self->interfaceLen = (int32_t)ifLen;
+                break;
+            }
+            case '-': {
+                prevOpt = '\0';
+                goto optsDone;
+            }
+            case '\0': {
+                if (arg[0] != '-') goto optsDone;
+                while (*++arg != '\0') {
+                    switch (*arg) {
+                        case 'b': self->broadcast = true; break;
+                        default: {
+                            prevOpt = *arg;
+                            if (arg[1] != '\0') goto optsDone;
+                        }
+                    }
+                }
+                continue;
+            }
+            default: goto optsDone;
+        }
+        prevOpt = '\0';
+    }
+    optsDone:;
+    if (prevOpt != '\0') {
+        struct iovec_const print[] = {
+            { hc_STR_COMMA_LEN("Invalid option `") },
+            { &prevOpt, 1 },
+            { hc_STR_COMMA_LEN("`\n") },
+        };
+        sys_writev(STDOUT_FILENO, &print[0], hc_ARRAY_LEN(print));
+        return -1;
+    }
+    if (argc != 0) {
+        sys_write(STDOUT_FILENO, hc_STR_COMMA_LEN("Too many arguments\n"));
+        return -1;
+    }
+    return 0;
+}
---
+++ examples/udp/udp.c
@@ -0,0 +1,67 @@
+#include "hc/hc.h"
+#include "hc/debug.h"
+#include "hc/mem.c"
+#include "hc/math.c"
+#include "hc/util.c"
+#include "hc/compiler_rt/libc.c"
+#include "hc/linux/linux.h"
+#include "hc/linux/sys.c"
+#include "hc/linux/debug.c"
+#include "hc/linux/util.c"
+#include "hc/linux/helpers/_start.c"
+
+#include "options.c"
+
+static char buffer[65536] hc_ALIGNED(16);
+
+int32_t start(int32_t argc, char **argv, hc_UNUSED char **envp) {
+    struct options options;
+    if (options_init(&options, argc, argv) < 0) {
+        sys_write(STDOUT_FILENO, hc_STR_COMMA_LEN("\nUsage: udp [-b] [-B ADDRESS] [-p PORT] [-i INTERFACE]\n"));
+        return 1;
+    }
+
+    int32_t fd = sys_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (fd < 0) return 1;
+
+    int32_t one = 1;
+    int32_t status = sys_setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    if (status < 0) return 1;
+
+    if (options.broadcast) {
+        status = sys_setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &one, sizeof(one));
+        if (status < 0) return 1;
+    }
+
+    if (options.interface != NULL) {
+        status = sys_setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, options.interface, options.interfaceLen);
+        if (status < 0) {
+            debug_print("Failed to bind to device\n");
+            return 1;
+        }
+    }
+
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = hc_BSWAP16(options.port),
+    };
+    hc_MEMCPY(&addr.sin_addr[0], &options.bindAddress[0], sizeof(addr.sin_addr));
+    status = sys_bind(fd, &addr, sizeof(addr));
+    if (status < 0) {
+        debug_print("Failed to bind to address\n");
+        return 1;
+    }
+
+    uint64_t totalRead = 0;
+    for (;;) {
+        int64_t read = sys_recvfrom(fd, &buffer[0], sizeof(buffer), 0, NULL, NULL);
+        if (read == 0) break;
+        if (read < 0) {
+            debug_print("Read failed\n");
+            return 1;
+        }
+        totalRead += (uint64_t)read;
+        debug_printNum("Total received: ", (int64_t)totalRead, "\n");
+    }
+    return 0;
+}
