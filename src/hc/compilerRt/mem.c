28d64e4abfbcb3b7b86363bf2eed4b7f719941eb
henrik <>
2024-01-28 17:54:03 +0100

    Cleanup memmove
    Aarch64 assembly seems to have improved by this

--- src/hc/compilerRt/mem.c
+++ src/hc/compilerRt/mem.c
@@ -22,18 +22,16 @@ void *memset(void *dest, int32_t c, size_t n) hc_NO_BUILTIN {
 }
 
 void *memmove(void *dest, const void *src, size_t n) hc_NO_BUILTIN {
-    uint64_t temp1, temp2;
     char *d = dest;
     const char *s = src;
     if (n >= 16) {
+        uint128_t temp;
         if (d < s) {
             size_t iEnd = n & ~(size_t)15;
             size_t i = 0;
             do {
-                hc_MEMCPY(&temp1, s + i, 8);
-                hc_MEMCPY(&temp2, s + i + 8, 8);
-                hc_MEMCPY(d + i, &temp1, 8);
-                hc_MEMCPY(d + i + 8, &temp2, 8);
+                hc_MEMCPY(&temp, s + i, 16);
+                hc_MEMCPY(d + i, &temp, 16);
                 i += 16;
             } while (i < iEnd);
             n &= 15;
@@ -42,25 +40,26 @@ void *memmove(void *dest, const void *src, size_t n) hc_NO_BUILTIN {
         } else {
             do {
                 n -= 16;
-                hc_MEMCPY(&temp1, s + n, 8);
-                hc_MEMCPY(&temp2, s + n + 8, 8);
-                hc_MEMCPY(d + n, &temp1, 8);
-                hc_MEMCPY(d + n + 8, &temp2, 8);
+                hc_MEMCPY(&temp, s + n, 16);
+                hc_MEMCPY(d + n, &temp, 16);
             } while (n >= 16);
         }
     }
     if (n == 0) return dest;
     if (n >= 8) {
+        uint64_t temp1, temp2;
         hc_MEMCPY(&temp1, s, 8);
         hc_MEMCPY(&temp2, s + n - 8, 8);
         hc_MEMCPY(d, &temp1, 8);
         hc_MEMCPY(d + n - 8, &temp2, 8);
     } else if (n >= 4) {
+        uint32_t temp1, temp2;
         hc_MEMCPY(&temp1, s, 4);
         hc_MEMCPY(&temp2, s + n - 4, 4);
         hc_MEMCPY(d, &temp1, 4);
         hc_MEMCPY(d + n - 4, &temp2, 4);
     } else if (n >= 2) {
+        uint16_t temp1, temp2;
         hc_MEMCPY(&temp1, s, 2);
         hc_MEMCPY(&temp2, s + n - 2, 2);
         hc_MEMCPY(d, &temp1, 2);
