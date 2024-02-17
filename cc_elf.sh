03219c95893a20c4454c9cc9c085c885562377c1
henrik <>
2024-02-17 15:40:32 +0100

    Set stack size for wasm

--- cc_elf.sh
+++ cc_elf.sh
@@ -6,4 +6,4 @@ if test -n "$LLVM"; then llvm_prefix="$LLVM/bin/"; fi
 
 flags="$(cat "$script_dir/flags")"
 ABI="${ABI:-linux-elf}"
-"${llvm_prefix}clang" -I"$script_dir/src" $flags -target $ARCH-unknown-$ABI --ld-path="${llvm_prefix}ld.lld" -Wl,-dynamic-linker="" -Wl,--build-id=none "$@"
+"${llvm_prefix}clang" -I"$script_dir/src" $flags -target $ARCH-unknown-$ABI --ld-path="${llvm_prefix}ld.lld" -Wl,-dynamic-linker="" -Wl,--build-id=none -Wl,-znognustack "$@"
