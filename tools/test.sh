781af56248a48a3508c886e0f4df708e6666548c
henrik <>
2024-07-22 15:53:01 +0200

    Tiny test.sh fix

--- tools/test.sh
+++ tools/test.sh
@@ -3,7 +3,7 @@ set -e
 script_dir="$(cd -- "${0%/*}/" && pwd)"
 root_dir="$script_dir/.."
 
-fails="$(find "$root_dir" -name "build.sh" -type f -exec sh -c 'echo "$1"; "$1" || echo x >&3' sh {} \; 3>&1 >&2)"
+fails="$(find "$(cd -- "$root_dir" && pwd)" -name "build.sh" -type f -exec sh -c 'echo "$1"; "$1" || echo x >&3' sh {} \; 3>&1 >&2)"
 test -z "$fails"
 
 . "$root_dir/tools/hostbuild.sh"
